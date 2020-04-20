237520004638fab1b2a6ddad22527e9a925ff107
Sammy Sloan <smileysammy3@gmail.com>
2020-04-19 22:32:03 -0600

    got the scores to all appear and made it so everything would dissappear when you click clear history

--- highscore.js
+++ highscore.js
@@ -1,11 +1,16 @@
-var myHighScores = JSON.parse(localStorage.getItem("scores"));
-var highScoreListEl = document.getElementById("high-score-list");
+var myHighScores = JSON.parse(localStorage.getItem("scores") || "[]");
 
-for (i = 0; i < myHighScores.length; i++) {
-    highScoreListEl.innerHTML("Initials: " + myHighScores[i].initials + "   Score: " + myHighScores[i].highScore);
+console.log(myHighScores);
+
+for (var i = 0; i < myHighScores.length; i++) {
+    var highScoreList = document.getElementById("high-scores-list");
+    var newLi = document.createElement("li")
+    newLi.textContent = "Intials: " + myHighScores[i].initials + " ---- Score: " + myHighScores[i].highScore;
+    highScoreList.append(newLi);
 }
 
 var clearBtn = document.getElementById("clear-button");
 clearBtn.addEventListener("click", function () {
     localStorage.clear();
+    location.reload();
 });
